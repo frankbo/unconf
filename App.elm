27eff957649e91d5ee64af53da2e05dd8324e674
Frank Boeddeker <frank.boeddeker@gmx.net>
2018-01-13 06:06:17 +0100

    Run elm-format

--- App.elm
+++ App.elm
@@ -1,21 +1,34 @@
-import Html exposing (..)
+module Main exposing (..)
 
+import Html exposing (..)
 import Navigation exposing (Location)
 import UrlParser exposing (..)
 
 
 -- APP
+
+
 main : Program Never Model Msg
 main =
-  Navigation.program OnLocationChange { init = init, view = view, update = update, subscriptions = subscriptions }
+    Navigation.program OnLocationChange { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
 
 -- MODEL
-type alias Model = { route : Route }
 
-type Msg = OnLocationChange Location
+
+type alias Model =
+    { route : Route }
+
+
+type Msg
+    = OnLocationChange Location
+
 
 initialModel : Route -> Model
-initialModel route = { route = route }
+initialModel route =
+    { route = route }
+
 
 init : Location -> ( Model, Cmd Msg )
 init location =
@@ -26,7 +39,6 @@ init location =
         ( initialModel currentRoute, Cmd.none )
 
 
-
 view : Model -> Html Msg
 view model =
     div []
@@ -35,39 +47,47 @@ view model =
 
 page : Model -> Html Msg
 page model =
-   case model.route of
-       Home ->
+    case model.route of
+        Home ->
             homeView
-       NotFound ->
-           notFoundView
+
+        NotFound ->
+            notFoundView
 
 
 homeView : Html Msg
 homeView =
     div [] [ text "Hello Woop" ]
 
+
 notFoundView : Html Msg
 notFoundView =
     div [] [ text "Hello Not Found" ]
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         OnLocationChange location ->
-           let
-               newRoute =
-                   parseLocation location
-           in
-               ( { model | route = newRoute }, Cmd.none )
+            let
+                newRoute =
+                    parseLocation location
+            in
+                ( { model | route = newRoute }, Cmd.none )
+
 
 subscriptions : model -> Sub msg
-subscriptions = \_ -> Sub.none
+subscriptions =
+    \_ -> Sub.none
+
+
 
 -- Routing
-type Route =
-    Home
-    | NotFound
 
+
+type Route
+    = Home
+    | NotFound
 
 
 matchers : Parser (Route -> a) a
